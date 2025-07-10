1c492a141aa02f9df67415275adec849616f1871
unknown <mrartur0074@gmail.com>
2025-07-10 16:55:42 +0500

    Hide navbar

--- frontend/src/components/navbar/Navbar.tsx
+++ frontend/src/components/navbar/Navbar.tsx
@@ -14,14 +14,14 @@ const Navbar = () => {
         <div className="notification">
           <img src="/notifications.svg" alt="" />
           <span>1</span>
-        </div> */}
+        </div> 
         <div className="user">
           <img
             src="https://images.pexels.com/photos/11038549/pexels-photo-11038549.jpeg?auto=compress&cs=tinysrgb&w=1600&lazy=load"
             alt=""
           />
           <span>Jane</span>
-        </div>
+        </div>*/}
         {/* <img src="/settings.svg" alt="" className="icon" /> */}
       </div>
     </div>
